c834f6f80313aa2644ff8a82cf730266abcf8c06
Maxim Khan-Magomedov <maxim.km@gmail.com>
2015-11-21 19:18:59 +0300

    Specs for category

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,5 +1,32 @@
 require 'rails_helper'
 
 RSpec.describe Category, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validation' do
+    it 'passes with valid attributes' do
+      category = build :category
+      expect(category).to be_valid
+    end
+
+    it 'fails without name' do
+      category = build :category, name: ' '
+      expect(category).not_to be_valid
+    end
+
+    it 'fails without slug' do
+      category = build :category, slug: ' '
+      expect(category).not_to be_valid
+    end
+
+    it 'fails with non-unique name' do
+      existing = create :category
+      category = build :category, name: existing.name
+      expect(category).not_to be_valid
+    end
+
+    it 'fails with non-unique slug' do
+      existing = create :category
+      category = build :category, slug: existing.slug
+      expect(category).not_to be_valid
+    end
+  end
 end
