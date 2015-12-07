edf4ee51f07e510550e0442340f838e3aee23b75
Maxim Khan-Magomedov <maxim.km@gmail.com>
2015-12-07 14:56:55 +0300

    Added annotations

--- app/models/order.rb
+++ app/models/order.rb
@@ -7,6 +7,13 @@ class Order < ActiveRecord::Base
   validates_presence_of :number
   after_initialize :generate_number
 
+  # Add item to order
+  #
+  # Increments value of quantity in OrderItem for this order.
+  # @see OrderItem#add
+  #
+  # @param [Item] item
+  # @param [Integer] quantity
   def add_item(item, quantity = 1)
     order_item = self.order_items.find_by item: item, price: item.price
     if order_item.is_a? OrderItem
@@ -17,6 +24,12 @@ class Order < ActiveRecord::Base
     recalculate!
   end
 
+  # Remove item from order
+  #
+  # Decrements value of quantity in OrderItem for this order
+  # @see OrderItem#remove
+  #
+  # @param [Item] item
   def remove_item(item)
     order_item = self.order_items.where(item: item).order('price asc').first
     if order_item.is_a? OrderItem
@@ -27,6 +40,7 @@ class Order < ActiveRecord::Base
 
   private
 
+  # Generate order number
   def generate_number
     if self.id.nil?
       last_part   = (Order.count + 1).to_s.rjust(3, '0')
@@ -34,6 +48,7 @@ class Order < ActiveRecord::Base
     end
   end
 
+  # Recalculate price and item count based on OrderItem data for this order
   def recalculate!
     attributes = { price: 0, item_count: 0 }
     self.order_items(true).map do |order_item|
--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -4,10 +4,18 @@ class OrderItem < ActiveRecord::Base
   belongs_to :order
   validates_presence_of :order_id
 
+  # Increment quantity
+  #
+  # @param [Integer] quantity
   def add(quantity = 1)
     increment! :quantity, quantity
   end
 
+  # Decrement quantity
+  #
+  # Removes link if quantity becomes less than 1
+  #
+  # @param [Integer] quantity
   def remove(quantity = 1)
     decrement! :quantity, quantity
     destroy if self.quantity < 1
