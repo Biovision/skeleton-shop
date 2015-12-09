fb6b8abbc9fc91bccc5bbfa152145415006dc040
Maxim Khan-Magomedov <maxim.km@gmail.com>
2015-12-09 23:29:57 +0300

    Specs for shop::items_controller

--- spec/controllers/shop/items_controller_spec.rb
+++ spec/controllers/shop/items_controller_spec.rb
@@ -1,5 +1,29 @@
 require 'rails_helper'
 
-RSpec.describe Shop::ItemsController, type: :controller do
-  pending
+RSpec.describe Shop::ItemsController, type: :controller, focus: true do
+  context 'when order_id is not set in session' do
+    describe 'post create' do
+      it 'creates new order'
+      it 'calls order#add_item'
+      it 'redirects to item page'
+    end
+
+    describe 'delete destroy' do
+      it 'creates new order'
+      it 'calls order#remove_item'
+      it 'redirects to item page'
+    end
+  end
+
+  context 'when order_id is set in session' do
+    describe 'post create' do
+      it 'calls order#add_item'
+      it 'redirects to item page'
+    end
+    
+    describe 'delete destroy' do
+      it 'calls order#remove_item'
+      it 'redirects to item page'
+    end
+  end
 end
